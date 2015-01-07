b4c9d0ad586114caaeffb828c1623064cb610096
Elena Boyd <elenaeboyd@gmail.com>
2015-01-07 14:25:15 -0600

    Initial Commit

---
+++ PointsByDistance/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PointsByDistance
+//
+//  Created by Elena Boyd on 1/7/15.
+//  Copyright (c) 2015 Whole Foods. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
